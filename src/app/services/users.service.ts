d47df53805c086386f91cf0e0c7577d4b8f9e27c
dvdgm12-arch <dvdgm12@gmail.com>
2026-03-16 15:21:51 +0100

    SEGUIMOS CRITERIO 3 DEFINIMOS METODOS CHASIS SERVICIO

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,13 +1,41 @@
 import { HttpClient } from '@angular/common/http';
 
+/*
 export class UsersService {
-    private baseUrl: string = 'https://peticiones.online/users';
+    private baseUrl: string = 'https://peticiones.online/api/users';
 
     constructor(private http: HttpClient) { }
 
     getAll() { // GET /users }
-    getById(id: string) { /*GET /users/id */ }
-    insert(user: User) { /* POST /users */ }
-    update(user: User) { /* PUT /users/id */ }
-    delete(id: string) { /* DELETE /users/id */ }
+    /*getById(id: string) { /*GET /users/id */ }
+     /*insert(user: User) { /* POST /users */ }
+    /* update(user: User) { /* PUT /users/id */ }
+     /*delete(id: string) { /* DELETE /users/id */ }
+//}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UsersService {
+    private htttpClient = inject(HttpClient);
+    private baseUrl: string = 'https://peticiones.online/api/users';
+
+    constructor() {
+    }
+
+    // DEVOLV TODOS LOS USUARIOS
+    getAll() {
+        console.log('Llamando a getall...');
+    }
+
+    // DEVOLV USUARIO POR ID
+    getById(id: string) {
+        console.log('Llamando a getById con id...');
+    }
+
+    //BORRAR USUARIO
+    delete(id: string) {
+        console.log('Borrando usuario...');
+    }
 }
+
