318d8370df1e8ddfe499529e4173742804057bff
dvdgm12-arch <dvdgm12@gmail.com>
2026-03-20 17:55:19 +0100

    LOGICA CRITERIO 4 GETALL COMPLETADA

--- src/app/interfaces/user.interface.ts
+++ src/app/interfaces/user.interface.ts
@@ -3,6 +3,16 @@ export interface IUser {
     id?: number;
     first_name: string;
     last_name: string;
+    username: string;
     email: string;
     image: string;
+    }
+
+export interface IUserResponse {
+  page: number;
+  per_page: number;
+  total: number;
+  total_pages: number;
+  results: IUser[];
 }
+
