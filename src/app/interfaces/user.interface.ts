4c3710485ea7043bf4971c1253fec62b7761b5be
dvdgm12-arch <dvdgm12@gmail.com>
2026-03-20 16:15:16 +0100

    LOGICA SERVICIOS CONFIGURADA

--- src/app/interfaces/user.interface.ts
+++ src/app/interfaces/user.interface.ts
@@ -1,4 +1,4 @@
-export interface User {
+export interface IUser {
     _id?: string;
     id?: number;
     first_name: string;
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,89 +1,35 @@
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { inject } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { IUser } from "../interfaces/user.interface";
+import { firstValueFrom } from "rxjs";
 
-/*
-export class UsersService {
-    private baseUrl: string = 'https://peticiones.online/api/users';
-
-    constructor(private http: HttpClient) { }
-
-    getAll() { // GET /users }
-    /*getById(id: string) { /*GET /users/id */ }
-     /*insert(user: User) { /* POST /users */ }
-    /* update(user: User) { /* PUT /users/id */ }
-     /*delete(id: string) { /* DELETE /users/id */ }
-//}
-
-/*@Injectable({
-    providedIn: 'root'
-})
-export class UsersService {
-    private htttpClient = inject(HttpClient);
-    private baseUrl: string = 'https://peticiones.online/api/users';
-
-    constructor() {
-    }
-
-    // DEVOLV TODOS LOS USUARIOS
-    getAll() {
-        console.log('Llamando a getall...');
-    }
-
-    // DEVOLV USUARIO POR ID
-    getById(id: string) {
-        console.log('Llamando a getById con id...');
-    }
-
-    //BORRAR USUARIO
-    delete(id: string) {
-        console.log('Borrando usuario...');
-    }
-}*/
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
+
 export class UsersService {
   private httpClient = inject(HttpClient);
-  private baseUrl: string = 'https://peticiones.online/api/users';
+  private baseUrl = 'https://peticiones.online/api/users';
 
-  // Devolvemos Observables directamente
-  getAll(): Observable<any> {
-    return this.httpClient.get<any>(this.baseUrl);
+  // OBTENER TODOS LOS USUARIOS (GET)
+  getAll(): Promise<IUser[]> {
+    return firstValueFrom(this.httpClient.get<IUser[]>(this.baseUrl));
   }
 
-  getById(id: string): Observable<User> {
-    return this.httpClient.get<User>(`${this.baseUrl}/${id}`);
+  // CREAR UN USUARIO (POST)
+  create(user: IUser): Promise<IUser> {
+    return firstValueFrom(this.httpClient.post<IUser>(this.baseUrl, user));
   }
 
-  insert(user: User): Observable<User> {
-    return this.httpClient.post<User>(this.baseUrl, user);
-  }
-
-  update(user: User): Observable<User> {
-    return this.httpClient.put<User>(`${this.baseUrl}/${user._id}`, user);
-  }
-
-  delete(id: string): Observable<any> {
-    return this.httpClient.delete<any>(`${this.baseUrl}/${id}`);
+  // ACTUALIZAR UN USUARIO (PUT)
+  update(id: number, user: IUser): Promise<IUser> {
+    return firstValueFrom(this.httpClient.put<IUser>(`${ this.baseUrl }/$ { id }`, user)); // USAMOS USER ID PARA RUTA DINAMICA
   }
 }
 
-// En el componente
-users = signal<User[]>([]); // Creamos una Signal
 
-async ngOnInit() {
-  // Llamamos al servicio y actualizamos la Signal
-  this.usersService.getAll().subscribe((data: any) => {
-    this.users.set(data.results); // 'set' actualiza el valor y la pantalla reacciona
-  });
-}
 
-// En el componente
-users = signal<User[]>([]); // Creamos una Signal
-
-async ngOnInit() {
-  // Llamamos al servicio y actualizamos la Signal
-  this.usersService.getAll().subscribe((data: any) => {
-    this.users.set(data.results); // 'set' actualiza el valor y la pantalla reacciona
-  });
-}
+  
+  
